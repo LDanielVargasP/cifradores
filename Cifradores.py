b8cb0f0015c74d3009bab1b7d6ae089f98d76aa0
PCLFRANKO <pacoygus@gmail.com>
2022-09-23 14:10:55 -0500

    llave

--- Cifradores.py
+++ Cifradores.py
@@ -1,1 +1,26 @@
-print("Hola Wuicho")
+from random import randint
+
+
+frase = input('Ingresa la frase: ')
+print('La frase es: '+frase)
+
+
+def cifrarOneTimePad(frase):
+    #print(frase+' desde funcion' )
+    abecedario = 'abcdefghijklmnñopqrstuvwxyzáéíóúü'
+    llave = ''
+
+    for x in range(len(frase)): #Crea la llave del tamanio de la frase
+        if frase[x] == ' ':
+            llave += ' ' 
+            x=+1
+        else:
+            k = randint(0,len(abecedario)-1)
+            llave += abecedario[k]
+        #print(x)
+
+    print(llave)
+
+cifrarOneTimePad(frase)
+
+
