5a15a94080c9514537873e1b2b30b5411e59d4c9
jesusbaltazar13 <israel_baltazar_13@hotmail.com>
2022-09-25 00:13:42 -0500

    Update hill.py

--- hill.py
+++ hill.py
@@ -1,1 +1,15 @@
-print("Codigo hill")
+frase = str(input("Coloque la frase a cifrar en mayusculas: "))
+print(f'frase {frase}')
+
+
+diccionario_encriptacion = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9,
+                            'K': 10, 'L': 11, 'M': 12, 'N': 13, 'Ñ': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18,
+                            'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26, ' ': 27}
+
+matrix = []
+
+for i in range(0, len(frase)):
+            matrix.append(diccionario_encriptacion[frase[i]])
+
+
+print(f'{matrix}')
