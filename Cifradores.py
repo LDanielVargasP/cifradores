d780668d1c689c85febc5443be07573c9d0d1923
jesusbaltazar13 <israel_baltazar_13@hotmail.com>
2022-09-27 15:19:48 -0500

    Update Cifradores.py

--- Cifradores.py
+++ Cifradores.py
@@ -1,219 +1,3 @@
-from ast import match_case
-import numpy as np
-
-alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-validacion = False
-validacion2 = False
-
-opcion = input("Que desea hacer:\nEncryptar = 1\nDesencriptar = 2\n ")
-
-match opcion:
-    case "1":
-        validacion = True
-        if validacion:
-            mensaje = input("\nIngrese el mensaje (debe tener 6 letras y en MAYUSCULAS): ")
-        if validacion and len(mensaje) != 6:
-            print("El mensaje debe de tener 6 caracteres")
-            validacion = False
-
-
-    case "2":
-        validacion2 = True
-        if validacion2:
-            mensajeEncryptado = input("\nIngrese el mensaje a decifrar(debe tener 6 letras y en MAYUSCULAS): ")
-
-        if validacion and len(mensajeEncryptado) != 6:
-            print("El mensaje debe de tener 6 caracteres")
-            validacion = False
-
-def MetodoHillEncryptar(alfabeto, mensaje):
-    #Declaracion de las matrices
-    K = [
-        [1, 0, 1],
-        [0, 2, 1],
-        [1, 5, 4],
-    ]
-    M1 = [
-        [0],
-        [0],
-        [0]
-    ]
-    M2 = [
-        [0],
-        [0],
-        [0]
-    ]
-    x = [
-        [0],
-        [0],
-        [0]
-    ]
-    y = [
-        [0],
-        [0],
-        [0]
-    ]
-
-    
-    # insertar las primeras tres letras  del mensaje en la matriz M1
-    letraMensaje = 0
-    for i in range(0, 3):
-        for j in range(0, len(alfabeto)):
-            if mensaje[letraMensaje] == alfabeto[j]:
-                M1[i][0] = j
-                break
-        letraMensaje += 1
-
-    for i in range(0, 3):
-        for j in range(0, len(alfabeto)):
-            if mensaje[letraMensaje] == alfabeto[j]:
-                M2[i][0] = j
-                break
-        letraMensaje += 1
-
-    #imprimir las matrices
-    print("Estas seran las matrices que se usaran: ")
-
-    print("matriz K: ")
-    for line in K:
-        print(' '.join(map(str, line)))
-    print("\n")
-
-    print("Matriz M1")
-    for line in M1:
-        print(' '.join(map(str, line)))
-    print("\n")
-
-    print("Matriz M2")
-    for line in M2:
-        print(' '.join(map(str, line)))
-    print("\n")
-
-    # Se multiplican las natrices con la libreria numpy
-    x = np.dot(K, M1)
-    y = np.dot(K, M2)
-
-    # Variable para obtener la cantidad de letras en el abecedario
-    tamanoAbecedario = len(alfabeto)
-
-    # Ciclo para obtner el resultado de los modulos
-    for i in range(0, 3):
-        x[i][0] = x[i][0] % tamanoAbecedario
-        y[i][0] = y[i][0] % tamanoAbecedario
-
-    # ciclos para obtener el criptograma
-    criptograma = ""
-    for i in range(0, 3):
-        for j in range (0, len(alfabeto)):
-            if x[i][0] == j:
-                criptograma += alfabeto[j]
-    criptograma += ""
-
-    for i in range(0, 3):
-        for j in range(0, len(alfabeto)):
-            if y[i][0] == j:
-                criptograma += alfabeto[j]
-
-    return criptograma
-
-def MetodoHillDesencryptar(alfabeto, mensaje2):
-    K = [
-        [3, 5, -2],
-        [1, 3, -1],
-        [-2, -5, 2],
-    ]
-    M1 = [
-        [0],
-        [0],
-        [0]
-    ]
-    M2 = [
-        [0],
-        [0],
-        [0]
-    ]
-    x = [
-        [0],
-        [0],
-        [0]
-    ]
-    y = [
-        [0],
-        [0],
-        [0]
-    ]
-
-    
-    # insertar las primeras tres letras  del mensaje en la matriz M1
-    letraMensaje = 0
-    for i in range(0, 3):
-        for j in range(0, len(alfabeto)):
-            if mensajeEncryptado[letraMensaje] == alfabeto[j]:
-                M1[i][0] = j
-                break
-        letraMensaje += 1
-
-    for i in range(0, 3):
-        for j in range(0, len(alfabeto)):
-            if mensajeEncryptado[letraMensaje] == alfabeto[j]:
-                M2[i][0] = j
-                break
-        letraMensaje += 1
-
-    #imprimir las matrices
-    print("Estas seran las matrices que se usaran: ")
-
-    print("matriz K: ")
-    for line in K:
-        print(' '.join(map(str, line)))
-    print("\n")
-
-    print("Matriz M1")
-    for line in M1:
-        print(' '.join(map(str, line)))
-    print("\n")
-
-    print("Matriz M2")
-    for line in M2:
-        print(' '.join(map(str, line)))
-    print("\n")
-
-    # Se multiplican las natrices con la libreria numpy
-    x = np.dot(K, M1)
-    y = np.dot(K, M2)
-
-    # Variable para obtener la cantidad de letras en el abecedario
-    tamanoAbecedario = len(alfabeto)
-
-    # Ciclo para obtner el resultado de los modulos
-    for i in range(0, 3):
-        x[i][0] = x[i][0] % tamanoAbecedario
-        y[i][0] = y[i][0] % tamanoAbecedario
-
-    # ciclos para obtener el criptograma
-    criptograma = ""
-    for i in range(0, 3):
-        for j in range (0, len(alfabeto)):
-            if x[i][0] == j:
-                criptograma += alfabeto[j]
-    criptograma += ""
-
-    for i in range(0, 3):
-        for j in range(0, len(alfabeto)):
-            if y[i][0] == j:
-                criptograma += alfabeto[j]
-
-    return criptograma
-    
-
-
-if validacion:
-    print("\nResultado del criptograma: " + MetodoHillEncryptar(alfabeto, mensaje))
-    validacion = False
-if validacion2:
-    print("\nResultado de desencriptacion: " + MetodoHillDesencryptar(alfabeto, mensajeEncryptado))
-    validacion = False
 '''
 Integrantes:
 Luis Daniel Vargas Peña
@@ -223,6 +7,7 @@ Francisco Obledo Garcia
 import os
 import random 
 from random import randint
+import numpy as np
 
             #variables Playfair
 #-----------------------------------------------------
@@ -400,8 +185,236 @@ def menuPlayfair():
         print('Opcion NO valida intente de nuevo')
         menuPlayfair()
 
+
+#                 Metodo Hill
 #-----------------------------------------------------
 
+alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+mensaje = ""
+mensajeEncryptado = ""
+
+
+def MetodoHillEncryptar(alfabeto, mensaje):
+    #Declaracion de las matrices
+    K = [
+        [1, 0, 1],
+        [0, 2, 1],
+        [1, 5, 4],
+    ]
+    M1 = [
+        [0],
+        [0],
+        [0]
+    ]
+    M2 = [
+        [0],
+        [0],
+        [0]
+    ]
+    x = [
+        [0],
+        [0],
+        [0]
+    ]
+    y = [
+        [0],
+        [0],
+        [0]
+    ]
+
+    
+    # insertar las primeras tres letras  del mensaje en la matriz M1
+    letraMensaje = 0
+    for i in range(0, 3):
+        for j in range(0, len(alfabeto)):
+            if mensaje[letraMensaje] == alfabeto[j]:
+                M1[i][0] = j
+                break
+        letraMensaje += 1
+
+    for i in range(0, 3):
+        for j in range(0, len(alfabeto)):
+            if mensaje[letraMensaje] == alfabeto[j]:
+                M2[i][0] = j
+                break
+        letraMensaje += 1
+
+    #imprimir las matrices
+    print("Estas seran las matrices que se usaran: ")
+
+    print("matriz K: ")
+    for line in K:
+        print(' '.join(map(str, line)))
+    print("\n")
+
+    print("Matriz M1")
+    for line in M1:
+        print(' '.join(map(str, line)))
+    print("\n")
+
+    print("Matriz M2")
+    for line in M2:
+        print(' '.join(map(str, line)))
+    print("\n")
+
+    # Se multiplican las natrices con la libreria numpy
+    x = np.dot(K, M1)
+    y = np.dot(K, M2)
+
+    # Variable para obtener la cantidad de letras en el abecedario
+    tamanoAbecedario = len(alfabeto)
+
+    # Ciclo para obtner el resultado de los modulos
+    for i in range(0, 3):
+        x[i][0] = x[i][0] % tamanoAbecedario
+        y[i][0] = y[i][0] % tamanoAbecedario
+
+    # ciclos para obtener el criptograma
+    criptograma = ""
+    for i in range(0, 3):
+        for j in range (0, len(alfabeto)):
+            if x[i][0] == j:
+                criptograma += alfabeto[j]
+    criptograma += ""
+
+    for i in range(0, 3):
+        for j in range(0, len(alfabeto)):
+            if y[i][0] == j:
+                criptograma += alfabeto[j]
+
+    return criptograma
+
+def MetodoHillDesencryptar(alfabeto, mensajeEncryptado):
+    K = [
+        [3, 5, -2],
+        [1, 3, -1],
+        [-2, -5, 2],
+    ]
+    M1 = [
+        [0],
+        [0],
+        [0]
+    ]
+    M2 = [
+        [0],
+        [0],
+        [0]
+    ]
+    x = [
+        [0],
+        [0],
+        [0]
+    ]
+    y = [
+        [0],
+        [0],
+        [0]
+    ]
+
+    
+    # insertar las primeras tres letras  del mensaje en la matriz M1
+    letraMensaje = 0
+    for i in range(0, 3):
+        for j in range(0, len(alfabeto)):
+            if mensajeEncryptado[letraMensaje] == alfabeto[j]:
+                M1[i][0] = j
+                break
+        letraMensaje += 1
+
+    for i in range(0, 3):
+        for j in range(0, len(alfabeto)):
+            if mensajeEncryptado[letraMensaje] == alfabeto[j]:
+                M2[i][0] = j
+                break
+        letraMensaje += 1
+
+    #imprimir las matrices
+    print("Estas seran las matrices que se usaran: ")
+
+    print("matriz K: ")
+    for line in K:
+        print(' '.join(map(str, line)))
+    print("\n")
+
+    print("Matriz M1")
+    for line in M1:
+        print(' '.join(map(str, line)))
+    print("\n")
+
+    print("Matriz M2")
+    for line in M2:
+        print(' '.join(map(str, line)))
+    print("\n")
+
+    # Se multiplican las natrices con la libreria numpy
+    x = np.dot(K, M1)
+    y = np.dot(K, M2)
+
+    # Variable para obtener la cantidad de letras en el abecedario
+    tamanoAbecedario = len(alfabeto)
+
+    # Ciclo para obtner el resultado de los modulos
+    for i in range(0, 3):
+        x[i][0] = x[i][0] % tamanoAbecedario
+        y[i][0] = y[i][0] % tamanoAbecedario
+
+    # ciclos para obtener el criptograma
+    criptograma = ""
+    for i in range(0, 3):
+        for j in range (0, len(alfabeto)):
+            if x[i][0] == j:
+                criptograma += alfabeto[j]
+    criptograma += ""
+
+    for i in range(0, 3):
+        for j in range(0, len(alfabeto)):
+            if y[i][0] == j:
+                criptograma += alfabeto[j]
+
+    return criptograma
+    
+def menuHill():
+    print('''
+            Cifrado Hill
+
+    0) Salir
+    1) Encriptar
+    2) Desencriptar
+    ''')
+    y = input('Elija una opcion: ')
+
+    if y == '0':
+        return
+    elif y == '1':
+        os.system('cls')
+        print('Funcion Encriptar')
+        mensaje = input("Escribe la palabra a encriptar debe tener solo 6 letras : \n")
+        mensaje = mensaje.upper()
+        if len(mensaje) != 6:
+            print("Especificaciones incorrectas")
+            menuHill()
+        else:
+            print("\nResultado del criptograma: " + MetodoHillEncryptar(alfabeto, mensaje))
+            menuHill()
+    elif y == '2':
+        os.system('cls')
+        print('Funcion Desencriptar')
+        mensajeEncryptado = input("Ingrese el mensaje a decifrar debe tener solo 6 letras): \n")
+        mensajeEncryptado = mensajeEncryptado.upper()
+        if len(mensajeEncryptado) != 6:
+            print("Especificaciones incorrectas")
+            menuHill()
+        else:
+            print("\nResultado de desencriptacion: " + MetodoHillDesencryptar(alfabeto, mensajeEncryptado))
+            menuHill()
+    else:
+        os.system('cls')
+        print('Opcion NO valida intente de nuevo')
+        menuPlayfair()
+
+
+#                Metodo OTP 
+#----------------------------------------------------
 
 def verificarLenguaje(entrada):
 
@@ -502,7 +515,7 @@ def menuOTP():
         print('Opcion NO valida intente de nuevo')
         menuOTP()
 
-#----------------------------------------------------------
+
 if __name__ == '__main__':
     x = 1
     while x == 1:
@@ -525,6 +538,7 @@ if __name__ == '__main__':
         elif y == '2':
             os.system('cls')
             print('Cifrado de Hill')
+            menuHill()
         elif y == '3':
             os.system('cls')
             menuOTP()
